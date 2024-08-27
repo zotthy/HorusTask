cfec40cf586baf2e94e485c166035d260088bae3
zotthy <se05110800@gmail.com>
2024-08-27 20:46:39 +0200

    stream

--- src/main/java/app/Wall.java
+++ src/main/java/app/Wall.java
@@ -1,9 +1,10 @@
 package app;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Wall implements Structure {
 
@@ -15,22 +16,18 @@ public class Wall implements Structure {
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
-        return findBlocksByColor(color,blockList);
+        return findBlocksByColor(color, blockList);
     }
 
     private Optional<Block> findBlocksByColor(String color, List<Block> blocks) {
-        for (Block block : blocks) {
-            if (Objects.equals(block.getColor(), color)) {
-                return Optional.of(block);
-            }
-            if (block instanceof CompositeBlock) {
-                Optional<Block> found = findBlocksByColor(color, ((CompositeBlock) block).getBlocks());
-                if (found.isPresent()) {
-                    return found;
-                }
-            }
-        }
-        return Optional.empty();
+        return blocks.stream().flatMap(block -> {
+                    Stream<Block> blockStream = Objects.equals(block.getColor(), color) ? Stream.of(block) : Stream.empty();
+                    Stream<Block> blockInComposite = block instanceof CompositeBlock
+                            ? findBlocksByColor(color, ((CompositeBlock) block).getBlocks()).stream()
+                            : Stream.empty();
+                    return Stream.concat(blockStream,blockInComposite);
+                })
+                .findFirst();
     }
 
     @Override
@@ -39,17 +36,17 @@ public class Wall implements Structure {
     }
 
     private List<Block> findBlocksByMaterial(String material, List<Block> blockList) {
-        List<Block> foundBlocks = new ArrayList<>();
-        for (Block block : blockList) {
-            if (Objects.equals(block.getMaterial(), material)) {
-                foundBlocks.add(block);
-            }
-            if (block instanceof CompositeBlock) {
-                foundBlocks.addAll(findBlocksByMaterial(material, ((CompositeBlock) block).getBlocks()));
-            }
-        }
-        return foundBlocks;
+        return blockList.stream().flatMap(block -> {
+                    Stream<Block> blockStream = Objects.equals(block.getMaterial(), material) ? Stream.of(block) : Stream.empty();
+                    Stream<Block> blockInComposite = block instanceof CompositeBlock
+                            ? findBlocksByMaterial(material, ((CompositeBlock) block).getBlocks()).stream()
+                            : Stream.empty();
+
+                    return Stream.concat(blockStream, blockInComposite);
+                })
+                .collect(Collectors.toList());
     }
+
 
     @Override
     public int count() {
@@ -57,15 +54,11 @@ public class Wall implements Structure {
     }
 
     public int count(List<Block> blocks) {
-        int sum = 0;
-        for (Block block : blocks) {
-            if (block instanceof CompositeBlock) {
-                sum += count(((CompositeBlock) block).getBlocks());
-            }
-            else {
-                sum++;
-            }
-        }
-        return sum;
+        return blocks.stream()
+                .mapToInt(block -> block instanceof CompositeBlock
+                        ? count(((CompositeBlock) block).getBlocks())
+                        : 1)
+                .sum();
     }
+
 }
--- src/test/java/app/WallTest.java
+++ src/test/java/app/WallTest.java
@@ -1,7 +1,6 @@
 package app;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.Arrays;
